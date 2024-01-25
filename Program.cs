bac9821d1f31a2d2bbb1dddb898fcf20ebcbe633
George Chakhidze <0xfeeddeadbeef@gmail.com>
2024-01-25 22:25:38 +0400

    Create the project

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿
+Console.WriteLine(Environment.OSVersion.ToString());
+Console.WriteLine(Environment.Version.ToString());
+Console.WriteLine(System.Runtime.InteropServices.RuntimeInformation.FrameworkDescription);
+Console.WriteLine(System.Runtime.InteropServices.RuntimeInformation.RuntimeIdentifier);
+Console.WriteLine(System.Runtime.InteropServices.RuntimeEnvironment.GetSystemVersion());
